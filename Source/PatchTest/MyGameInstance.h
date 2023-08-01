9a99bab955487e6d1789a60f5a2672040b12be70
jh-210 <115517596+jh-210@users.noreply.github.com>
2023-08-01 10:46:54 +0900

    .

---
+++ Source/PatchTest/MyGameInstance.h
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "MyGameInstance.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FPatchCompleteDelegate, bool, Succeeded);
+
+/**
+ * 
+ */
+UCLASS()
+class PATCHTEST_API UMyGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+public:
+	/** Overrides */
+	virtual void Init() override;
+	virtual void Shutdown() override;
+
+	UFUNCTION(BlueprintPure, Category = "Patching|Stats")
+		void GetLoadingProgress(int32& BytesDownloaded, int32& TotalBytesToDownload, float& DownloadPercent, int32& ChunksMounted, int32& TotalChunksToMount, float& MountPercent) const;
+
+	UPROPERTY(BlueprintAssignable, Category = "Patching");
+		FPatchCompleteDelegate OnPatchComplete;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Patching")
+		TArray<int32> ChunkDownloadList;
+
+	UFUNCTION(BlueprintCallable, Category = "Patching")
+		bool PatchGame();
+
+
+protected:
+	//Tracks Whether or not our local manifest file is up to date with the one hosted on our website
+	bool bIsDownloadManifestUpToDate;
+
+	void OnManifestUpdateComplete(bool bSuccess);
+
+	/** Called when the chunk download process finishes */
+	void OnDownloadComplete(bool bSuccess);
+
+	/** Called whenever ChunkDownloader's loading mode is finished*/
+	void OnLoadingModeComplete(bool bSuccess);
+
+	/** Called when ChunkDownloader finishes mounting chunks */
+	void OnMountComplete(bool bSuccess);
+};
